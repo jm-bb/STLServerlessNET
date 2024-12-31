f8ea6cbd5543857449f50a9528a605dde5e2fbfd
James Magee <jmagee1@gmail.com>
2024-12-31 03:30:36 -0600

    Removing unused fields.

--- Entities/Web/Product.cs
+++ Entities/Web/Product.cs
@@ -22,17 +22,17 @@ public partial class Product
     [Column("prod_name", TypeName = "tinytext")]
     public string ProdName { get; set; } = null!;
 
-    [Column("custom_h2", TypeName = "tinytext")]
-    public string? CustomH2 { get; set; }
+    //[Column("custom_h2", TypeName = "tinytext")]
+    //public string? CustomH2 { get; set; }
 
     [Column("prod_short_description", TypeName = "tinytext")]
     public string? ProdShortDescription { get; set; }
 
-    [Column("prod_description", TypeName = "text")]
-    public string? ProdDescription { get; set; }
+    //[Column("prod_description", TypeName = "text")]
+    //public string? ProdDescription { get; set; }
 
-    [Column("prod_features", TypeName = "text")]
-    public string? ProdFeatures { get; set; }
+    //[Column("prod_features", TypeName = "text")]
+    //public string? ProdFeatures { get; set; }
 
     [Column("prod_sku")]
     [StringLength(25)]
@@ -78,81 +78,81 @@ public partial class Product
     [Column("length")]
     public float? Length { get; set; }
 
-    [Column("brochure", TypeName = "text")]
-    public string? Brochure { get; set; }
+    //[Column("brochure", TypeName = "text")]
+    //public string? Brochure { get; set; }
 
-    [Column("brochure_thumbnail")]
-    [StringLength(175)]
-    public string? BrochureThumbnail { get; set; }
+    //[Column("brochure_thumbnail")]
+    //[StringLength(175)]
+    //public string? BrochureThumbnail { get; set; }
 
-    [Column("instructions")]
-    [StringLength(175)]
-    public string? Instructions { get; set; }
+    //[Column("instructions")]
+    //[StringLength(175)]
+    //public string? Instructions { get; set; }
 
-    [Column("bracket_chart")]
-    [StringLength(175)]
-    public string? BracketChart { get; set; }
+    //[Column("bracket_chart")]
+    //[StringLength(175)]
+    //public string? BracketChart { get; set; }
 
-    [Column("flash_pattern")]
-    [StringLength(175)]
-    public string? FlashPattern { get; set; }
+    //[Column("flash_pattern")]
+    //[StringLength(175)]
+    //public string? FlashPattern { get; set; }
 
-    [Column("specifications_image")]
-    [StringLength(175)]
-    public string? SpecificationsImage { get; set; }
+    //[Column("specifications_image")]
+    //[StringLength(175)]
+    //public string? SpecificationsImage { get; set; }
 
-    [Column("visor_instructions")]
-    [StringLength(175)]
-    public string? VisorInstructions { get; set; }
+    //[Column("visor_instructions")]
+    //[StringLength(175)]
+    //public string? VisorInstructions { get; set; }
 
     [Column("product_type", TypeName = "enum('apparel','public','havis','reconditioned','clearance','giftcard','package','vehicles')")]
     public string? ProductType { get; set; }
 
-    [Column("show_admin", TypeName = "enum('Y','N')")]
-    public string? ShowAdmin { get; set; }
+    //[Column("show_admin", TypeName = "enum('Y','N')")]
+    //public string? ShowAdmin { get; set; }
 
     [Column("create_date", TypeName = "datetime")]
     public DateTime CreateDate { get; set; }
 
-    [Column("video_mini_thumbnail")]
-    [StringLength(255)]
-    public string? VideoMiniThumbnail { get; set; }
+    //[Column("video_mini_thumbnail")]
+    //[StringLength(255)]
+    //public string? VideoMiniThumbnail { get; set; }
 
-    [Column("video_main_thumbnail")]
-    [StringLength(255)]
-    public string? VideoMainThumbnail { get; set; }
+    //[Column("video_main_thumbnail")]
+    //[StringLength(255)]
+    //public string? VideoMainThumbnail { get; set; }
 
-    [Column("video_youtube_widget", TypeName = "text")]
-    public string? VideoYoutubeWidget { get; set; }
+    //[Column("video_youtube_widget", TypeName = "text")]
+    //public string? VideoYoutubeWidget { get; set; }
 
     [Column("update_date", TypeName = "datetime")]
     public DateTime? UpdateDate { get; set; }
 
-    [Column("dealer_list_price")]
-    public float? DealerListPrice { get; set; }
+    //[Column("dealer_list_price")]
+    //public float? DealerListPrice { get; set; }
 
-    [Column("dealer_sale_price")]
-    public float? DealerSalePrice { get; set; }
+    //[Column("dealer_sale_price")]
+    //public float? DealerSalePrice { get; set; }
 
-    [Column("dealer_international_sale_price")]
-    public float? DealerInternationalSalePrice { get; set; }
+    //[Column("dealer_international_sale_price")]
+    //public float? DealerInternationalSalePrice { get; set; }
 
-    [Column("dealer_international_list_price")]
-    public float? DealerInternationalListPrice { get; set; }
+    //[Column("dealer_international_list_price")]
+    //public float? DealerInternationalListPrice { get; set; }
 
-    [Column("dealerActive", TypeName = "enum('Y','N')")]
-    public string? DealerActive { get; set; }
+    //[Column("dealerActive", TypeName = "enum('Y','N')")]
+    //public string? DealerActive { get; set; }
 
-    [Column("patent_num", TypeName = "text")]
-    public string? PatentNum { get; set; }
+    //[Column("patent_num", TypeName = "text")]
+    //public string? PatentNum { get; set; }
 
-    [Column("sae_certification_num")]
-    [StringLength(50)]
-    public string? SaeCertificationNum { get; set; }
+    //[Column("sae_certification_num")]
+    //[StringLength(50)]
+    //public string? SaeCertificationNum { get; set; }
 
-    [Column("rohs_certification_num")]
-    [StringLength(50)]
-    public string? RohsCertificationNum { get; set; }
+    //[Column("rohs_certification_num")]
+    //[StringLength(50)]
+    //public string? RohsCertificationNum { get; set; }
 
     [Column("lightbar_type", TypeName = "enum('NO_LIGHTBAR','4_HEAD','4_HEAD_MULTICOLOR','6_HEAD','6_HEAD_MULTI','6_HEAD_MULTICOLOR','8_HEAD','8_HEAD_MULTI','8_HEAD_MULTICOLOR','ECLIPSE','ECLIPSE_MINI','MINI_BASIC_RAPTAR','MINI_BASIC_OCTO','MINI_PRIME_18','MINI_KFORCE_18','MINI_PRIME_BASIC_27','MINI_PRIME_27','MINI_KFORCE_27','MINI_CUST_18','FULL_LIGHTBAR_18','FULL_LIGHTBAR_KFORCE_18','FULL_LIGHTBAR_22','FULL_LIGHTBAR_KFORCE_22','FULL_LIGHTBAR_26','FULL_LIGHTBAR_KFORCE_26','FULL_LIGHTBAR_30','FULL_LIGHTBAR_KFORCE_30','FULL_LIGHTBAR_34','FULL_LIGHTBAR_KFORCE_34','SPLIT_ECLIPSE','FULL_LIGHTBAR_ARIES_18','FULL_LIGHTBAR_ARIES_MULTI_18','FULL_LIGHTBAR_ARIES_22','FULL_LIGHTBAR_ARIES_MULTI_22','FULL_LIGHTBAR_ARIES_26','FULL_LIGHTBAR_ARIES_MULTI_26','MICRO_BASIC_12','FULL_LIGHTBAR_MICRO_26','FULL_LIGHTBAR_MICRO_30','ARIES_MINI_18','ARIES_MINI_27','FULL_LIGHTBAR_PRIME_TOW_26','FULL_LIGHTBAR_KFORCE_TOW_26','FULL_LIGHTBAR_ARIES_TOW_26','FULL_LIGHTBAR_MICRO_TOW_30','SPLIT_ECLIPSE_X','MICRO_BASIC_21','REAR_SPLIT_RAPTOR','CeptorX_4_HEAD','CeptorX_4_STD_HEAD','CeptorX_6_HEAD','CeptorX_6_STD_HEAD','STD_RAPTOR','FULL_LIGHTBAR_STD_22','FULL_LIGHTBAR_KFORCE_TOW_63','SPLIT_ECLIPSE_MULTICOLOR','SPLIT_ECLIPSE_X_MULTICOLOR','STD_SPLIT_ECLIPSE_MULTICOLOR','MINI_PRIME_18_MULTICOLOR','MICRO_BASIC_12_MULTICOLOR','MINI_PRIME_27_MULTICOLOR','MICRO_BASIC_21_MULTICOLOR','FULL_LIGHTBAR_18_MULTICOLOR','FULL_LIGHTBAR_22_MULTICOLOR','FULL_LIGHTBAR_26_MULTICOLOR','FULL_LIGHTBAR_STD_22_MULTICOLOR','FULL_LIGHTBAR_MICRO_26_MULTICOLOR')")]
     public string? LightbarType { get; set; }
@@ -177,16 +177,16 @@ public partial class Product
     [Column("flat_shipping_fee")]
     public float? FlatShippingFee { get; set; }
 
-    [Column("video_thumbnail_image")]
-    [StringLength(350)]
-    public string? VideoThumbnailImage { get; set; }
+    //[Column("video_thumbnail_image")]
+    //[StringLength(350)]
+    //public string? VideoThumbnailImage { get; set; }
 
-    [Column("interactive_thumbnail_image")]
-    [StringLength(350)]
-    public string? InteractiveThumbnailImage { get; set; }
+    //[Column("interactive_thumbnail_image")]
+    //[StringLength(350)]
+    //public string? InteractiveThumbnailImage { get; set; }
 
-    [Column("interactive_player", TypeName = "text")]
-    public string? InteractivePlayer { get; set; }
+    //[Column("interactive_player", TypeName = "text")]
+    //public string? InteractivePlayer { get; set; }
 
     [Column("total_quantity", TypeName = "int(20)")]
     public int? TotalQuantity { get; set; }
@@ -202,29 +202,29 @@ public partial class Product
     [StringLength(100)]
     public string? Slug { get; set; }
 
-    [Column("meta_title")]
-    [StringLength(60)]
-    public string? MetaTitle { get; set; }
+    //[Column("meta_title")]
+    //[StringLength(60)]
+    //public string? MetaTitle { get; set; }
 
-    [Column("meta_keywords", TypeName = "text")]
-    public string? MetaKeywords { get; set; }
+    //[Column("meta_keywords", TypeName = "text")]
+    //public string? MetaKeywords { get; set; }
 
-    [Column("meta_description", TypeName = "text")]
-    public string? MetaDescription { get; set; }
+    //[Column("meta_description", TypeName = "text")]
+    //public string? MetaDescription { get; set; }
 
-    [Column("og_title")]
-    [StringLength(60)]
-    public string? OgTitle { get; set; }
+    //[Column("og_title")]
+    //[StringLength(60)]
+    //public string? OgTitle { get; set; }
 
-    [Column("og_keywords", TypeName = "text")]
-    public string? OgKeywords { get; set; }
+    //[Column("og_keywords", TypeName = "text")]
+    //public string? OgKeywords { get; set; }
 
-    [Column("og_description", TypeName = "text")]
-    public string? OgDescription { get; set; }
+    //[Column("og_description", TypeName = "text")]
+    //public string? OgDescription { get; set; }
 
-    [Column("og_image")]
-    [StringLength(255)]
-    public string? OgImage { get; set; }
+    //[Column("og_image")]
+    //[StringLength(255)]
+    //public string? OgImage { get; set; }
 
     [Column("slug_new")]
     [StringLength(100)]
@@ -233,35 +233,35 @@ public partial class Product
     [Column("category_for_url", TypeName = "int(11)")]
     public int? CategoryForUrl { get; set; }
 
-    [Column("amazon_up_charge")]
-    public float? AmazonUpCharge { get; set; }
+    //[Column("amazon_up_charge")]
+    //public float? AmazonUpCharge { get; set; }
 
-    [Column("amazon_display", TypeName = "enum('Y','N')")]
-    public string? AmazonDisplay { get; set; }
+    //[Column("amazon_display", TypeName = "enum('Y','N')")]
+    //public string? AmazonDisplay { get; set; }
 
-    [Column("ebay_up_charge")]
-    public float? EbayUpCharge { get; set; }
+    //[Column("ebay_up_charge")]
+    //public float? EbayUpCharge { get; set; }
 
-    [Column("ebay_display", TypeName = "enum('Y','N')")]
-    public string? EbayDisplay { get; set; }
+    //[Column("ebay_display", TypeName = "enum('Y','N')")]
+    //public string? EbayDisplay { get; set; }
 
-    [Column("show_in_stock", TypeName = "enum('Y','N')")]
-    public string? ShowInStock { get; set; }
+    //[Column("show_in_stock", TypeName = "enum('Y','N')")]
+    //public string? ShowInStock { get; set; }
 
-    [Column("video_thumbnail_2")]
-    [StringLength(150)]
-    public string? VideoThumbnail2 { get; set; }
+    //[Column("video_thumbnail_2")]
+    //[StringLength(150)]
+    //public string? VideoThumbnail2 { get; set; }
 
-    [Column("hover_image")]
-    [StringLength(150)]
-    public string? HoverImage { get; set; }
+    //[Column("hover_image")]
+    //[StringLength(150)]
+    //public string? HoverImage { get; set; }
 
     [Column("fishbowl_rule_id", TypeName = "int(11)")]
     public int FishbowlRuleId { get; set; }
 
-    [Column("cree_flag")]
-    [StringLength(3)]
-    public string CreeFlag { get; set; } = null!;
+    //[Column("cree_flag")]
+    //[StringLength(3)]
+    //public string CreeFlag { get; set; } = null!;
 
     [Column("warranty_flag")]
     [StringLength(3)]
@@ -271,9 +271,9 @@ public partial class Product
     [StringLength(3)]
     public string Warranty2Flag { get; set; } = null!;
 
-    [Column("moneyback_flag")]
-    [StringLength(3)]
-    public string MoneybackFlag { get; set; } = null!;
+    //[Column("moneyback_flag")]
+    //[StringLength(3)]
+    //public string MoneybackFlag { get; set; } = null!;
 
     [Column("freeshipping_flag")]
     [StringLength(3)]
@@ -308,16 +308,16 @@ public partial class Product
     [StringLength(250)]
     public string? OffroadCategory { get; set; }
 
-    [Column("gif_heading")]
-    [StringLength(100)]
-    public string? GifHeading { get; set; }
+    //[Column("gif_heading")]
+    //[StringLength(100)]
+    //public string? GifHeading { get; set; }
 
-    [Column("gif_image")]
-    [StringLength(250)]
-    public string? GifImage { get; set; }
+    //[Column("gif_image")]
+    //[StringLength(250)]
+    //public string? GifImage { get; set; }
 
-    [Column("whats_included", TypeName = "text")]
-    public string? WhatsIncluded { get; set; }
+    //[Column("whats_included", TypeName = "text")]
+    //public string? WhatsIncluded { get; set; }
 
     public ICollection<Cart> Carts { get; set; } = new List<Cart>();
 }
